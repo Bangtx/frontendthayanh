fd3bc642ff5de7b21b59d760b8fc2a4bae6e2ba9
Bangtx <72857647+Bangtx@users.noreply.github.com>
2021-09-11 15:35:32 +0700

    Add files via upload

---
+++ src/components/Dialog/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div v-show="show" class="overlay">
+    <div class="dialog">
+      <div class="dialog__content">
+        <h2 class="dialog__title">review</h2>
+        <p class="dialog__description">{{ review }}</p>
+      </div>
+      <hr />
+      <div class="dialog__footer">
+        <button @click="closeDialog" class="dialog__cancel">Cancel</button>
+<!--        <button class="dialog__confirm">Yes, delete it</button>-->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue'
+import { defineComponent } from '@vue/composition-api'
+
+const Dialog = defineComponent({
+  name: 'Dialog',
+  props: {
+    show: {
+      type: Boolean,
+      required: true
+    },
+    review: {
+      type: String,
+      required: true
+    }
+  },
+  setup(props, { emit }) {
+    const closeDialog = () => {
+      emit('closeDialog')
+    }
+    return {
+      closeDialog
+    }
+  }
+})
+
+export default Dialog
+</script>
+
+<style></style>
